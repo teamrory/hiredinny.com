af71cdc6660528a8e411f1e8b820686f72231c53
Andrew Pinzler <pinzler@gmail.com>
2013-06-04 00:00:30 -0400

    footer fix

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,10 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/jquery-1.9.1.min.js"><\/script>')</script>
+<script type="text/javascript">
+$(document).bind("mobileinit", function () {
+    $.mobile.ajaxEnabled = false;
+});
+</script>
 <script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
 <script src="/js/ss-social.js"></script>
 <script src="/js/ss-standard.js"></script>
