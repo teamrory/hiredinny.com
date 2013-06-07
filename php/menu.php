131de262f9504c928efbb092144ff6f2b57097df
Kennedy <kennedysgarage@gmail.com>
2013-06-07 01:42:49 -0400

    removed search from the menu

--- php/menu.php
+++ php/menu.php
@@ -12,16 +12,6 @@ for ($i = 0; $i < sizeof($objm); $i++) {
 
 <nav>
       <ul>
-          <li>
-            <form role="search" method="get" action="" id="search-form">
-                <div class="search-wrap">
-                    <div class="search-input">
-                        <input type="search" onwebkitspeechchange="startSearch()" x-webkit-grammar="builtin:search" x-webkit-speech="" placeholder="search..." value="" name="s" id="s">
-                        <input type="submit" value="&#x1F50E;" id="search-submit" class="ss-icon">
-                    </div><!--/search-input-->
-                </div><!--/search-wrap-->
-            </form>
-          </li>
           <!-- <li><strong><?php echo $_SESSION['fname'];?>'s - Jobs</strong></li> -->
           <li><strong>Your Jobs</strong></li>
           <?php echo $data; ?>
