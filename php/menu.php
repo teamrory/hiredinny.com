8a376fba51730cfebd409bcffa7b6081e9f727c9
Kennedy <kennedysgarage@gmail.com>
2013-06-07 02:08:04 -0400

    cleaning up the search box and adding an avatar

--- php/menu.php
+++ php/menu.php
@@ -8,13 +8,17 @@ for ($i = 0; $i < sizeof($objm); $i++) {
       $data.="<li><a href ='todos.php?jobid=".$objm[$i]->JobID."&compname=".$objm[$i]->CompanyName."&title=".$objm[$i]->Title."' rel='external'>".$objm[$i]->CompanyName." - ".$objm[$i]->Title."</a></li>";
 
   }
+
+$url = file_get_contents("http://hiredinny.com/private/getProfile/".$_SESSION['username'], true);
+$obj = json_decode($url);
+$userimg = $obj[0]->pictureUrl;
 ?>
 
 <nav>
       <ul>
-          <!-- <li><strong><?php echo $_SESSION['fname'];?>'s - Jobs</strong></li> -->
-          <li><strong>Your Jobs</strong></li>
-          <?php echo $data; ?>
+        <li><h3><img src="<?php echo $userimg; ?>"> Name<?php echo $_SESSION['fname'];?></h3></li>
+        <li><strong>Your Jobs</strong></li>
+        <?php echo $data; ?>
       </ul>
   </nav>
 <div id="page">
--- php/welcome.php
+++ php/welcome.php
@@ -20,13 +20,12 @@ include 'menu.php';
     <div class="container">
 
     <h4>Welcome, <?php echo $fname?>!</h4>
-    <br> <form name="input" action="jobresults.php" method="get">
-  Job Title Search: <input type="text" name="title">
-  <input type="submit" value="Submit">
-  </form>
+    <form class="form-search" name="input" action="jobresults.php" method="get">
+      <input type="text" class="  search-query" name="title" placeholder="Job Title Search">
+      <button type="submit" class="btn">Search</button>
+    </form>
 
     </div><!--/container-->
   </div><!--/main-->
-
 
 <?php include("footer.php"); ?>
